6b11274d90ad62887450bc55fc3425a1bd2c8060
LEE SANGJUN <lsj010115@gmail.com>
2025-09-29 19:15:27 +0900

    프로젝트 환경설정

---
+++ once-backend/src/main/java/com/once/once_backend/OnceApplication.java
@@ -0,0 +1,13 @@
+package com.once.once_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnceApplication.class, args);
+	}
+
+}
---
+++ once-backend/src/test/java/com/once/once_backend/OnceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.once.once_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
