7cf5a047bf33fb180ee1061b719c13bb6642aa10
KIM JIN MIN <maicle1@naver.com>
2025-11-13 20:23:18 +0900

    Update SecurityConfig.java

--- once-backend/src/main/java/com/once/config/SecurityConfig.java
+++ once-backend/src/main/java/com/once/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
                         .requestMatchers("/test/**").permitAll()
                         .requestMatchers("/groups/**").permitAll() // 그룹 기능 임시 공개
                         .requestMatchers("/error").permitAll()
+                        .requestMatchers("/calendar").permitAll()
 
                         // 인증 필요한 엔드포인트
                         .requestMatchers("/users/me").authenticated()
